acae725ae05909ee6ed83e54f39a0d9f52c99c0e
allievo7 <93972864+allievo7@users.noreply.github.com>
2021-11-10 11:36:19 +0100

    added application args input

--- Capitalizer/Program.cs
+++ Capitalizer/Program.cs
@@ -7,27 +7,33 @@ namespace Capitalizer
     {
         static void Main(string[] args)
         {
-            while (true)
-            {
-                Console.WriteLine("Enter the address of the chosen file:");
-                string fileAddress = Console.ReadLine();
-                string text = LoadTextFile(fileAddress);
-                Console.WriteLine(text);
-                Console.WriteLine();
-                string choice = MenuGenerator("Enter 'Point' to capitalize the first character of each sentence. \nEnter 'All' to capitalize the entire text file.");
-                string newText = Capitalize(choice, text);
-                Console.WriteLine();
-                string choice2 = MenuGenerator("Enter 'N' to save in a new text file. \nEnter 'R' to rewrite the original file. \nEnter 'B' to restart the application without saving.");
+            string fileAddress = args[0];
+            string choice = args[1];
+            string choice2 = args[2];
+            string text = LoadTextFile(fileAddress);
+            string newText = Capitalize(choice, text);
+            SaveTextFile(choice2, newText, fileAddress);
+            //while (true)
+            //{
+            //    Console.WriteLine("Enter the address of the chosen file:");
+            //    string fileAddress = Console.ReadLine();
+            //    string text = LoadTextFile(fileAddress);
+            //    Console.WriteLine(text);
+            //    Console.WriteLine();
+            //    string choice = MenuGenerator("Enter 'Point' to capitalize the first character of each sentence. \nEnter 'All' to capitalize the entire text file.");
+            //    string newText = Capitalize(choice, text);
+            //    Console.WriteLine();
+            //    string choice2 = MenuGenerator("Enter 'N' to save in a new text file. \nEnter 'R' to rewrite the original file. \nEnter 'B' to restart the application without saving.");
 
-                if (choice2 == "N")
-                {
-                    SaveTextFile(choice2, newText, fileAddress);
-                }
-                else if (choice2 == "R")
-                {
-                    SaveTextFile(choice2, newText, fileAddress);
-                }
-            }
+            //    if (choice2 == "N")
+            //    {
+            //        SaveTextFile(choice2, newText, fileAddress);
+            //    }
+            //    else if (choice2 == "R")
+            //    {
+            //        SaveTextFile(choice2, newText, fileAddress);
+            //    }
+            //}
         }
         static string Capitalize(string choice, string text)
         {
@@ -95,8 +101,8 @@ namespace Capitalizer
                 case "N":
                     string[] addressBody = address.Split("\\");
                     Console.WriteLine("Save as:");
-                    string fileName = Console.ReadLine();
-                    addressBody[addressBody.Length - 1] = fileName + ".txt";
+                    //string fileName = Console.ReadLine();
+                    addressBody[addressBody.Length - 1] = "copy.txt";
                     string newAddress = string.Join("\\", addressBody);
                     try
                     {
